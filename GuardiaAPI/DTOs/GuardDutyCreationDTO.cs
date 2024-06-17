63a628664a67a0c5c2d0ff8b9c073159bc53ad18
Cristian Perez <cristian.perez.domper@gmail.com>
2024-06-17 17:23:07 -0500

    Crea conexión con store procedure utilizando la libreria de dapper

--- GuardiaAPI/DTOs/GuardDutyCreationDTO.cs
+++ GuardiaAPI/DTOs/GuardDutyCreationDTO.cs
@@ -5,7 +5,7 @@ namespace GuardiaAPI.DTOs
     public class GuardDutyCreationDTO
     {
         [Required(ErrorMessage = "The field with name {0} is required")]
-        public int PersonId { get; set; }
+        public int? PersonId { get; set; }
         [Required(ErrorMessage = "The field with name {0} is required")]
         public int UserId { get; set; }
         [Required(ErrorMessage = "The field with name {0} is required")]
